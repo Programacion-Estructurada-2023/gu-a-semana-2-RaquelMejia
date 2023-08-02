d36eb9363e4c92f14341569d4da5863a3123d3b8
Raquelmejia <raquelflores0975@gmail.com>
2023-08-01 19:36:23 -0600

    Guia#2

---
+++ U20230815_Semana2.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //Declare 4 variables de tipo string que represente su nombre completo
+            string primerNombre= "Ana";
+            string segundoNombre= "Raquel";
+            string primerApellido= "Mejia";
+            string segundoApellido="Flores";
+
+            //Declare 3 variables de tipo integer que tengan relacion con la edad ya sea la suya y de sus padres
+            int miEdad = 24;
+            int edadMadre = 55;
+            int edadPadre = 57;
+
+            //Declare 3 variables de tipo float que representen sus gastos mas frecuentes
+            float cuotaUniversidad = 110.00f;
+            float gastoTransporte = 60.50f;
+            float gastoComida = 75.25f;
+
+            //Declare 2 variables de tipos boolean para representar su estado como estudiante en esta materia y otra si es becado
+            bool esEstudiante = true;
+            bool esBecado = false;
+
+            //Autoevaluacion 9
+
+
+        }
+        
+
+
+
+
+    }
+}
